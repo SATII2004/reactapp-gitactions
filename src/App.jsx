3eef9a4a796f4d2fda9157237fa606a8204a7d21
SATII2004 <2300030457@kluniversity.in>
2025-09-06 08:40:19 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import './style.css';
 function App() {
   return (
     <div className="container">
-      <h1>Welcome to My React Project - S105</h1>
+      <h1>Welcome to My React Project</h1>
+      <h1>CICD AND CLOUD DEVOPS COURSE</h1>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
     </div>
   );
