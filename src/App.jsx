76e9aafd04a72e7a70cff54104983b24dd37a489
SATII2004 <2300030457@kluniversity.in>
2025-09-06 10:06:32 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <h1>Welcome to My React Project</h1>
       <h1>CICD AND CLOUD DEVOPS COURSE</h1>
       <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
-      <p>Done with Git Pages And Git Actions</p>
+      <h1>Done with Git Pages And Git Actions</h1>
     </div>
   );
 }
